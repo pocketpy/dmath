2f69cf2118c46d2bbba0bd5b00fdacbcbb208684
blueloveTH <blueloveTH@foxmail.com>
2025-05-23 11:25:12 +0800

    fix ci

--- tests/main.c
+++ tests/main.c
@@ -1,7 +1,17 @@
 #include <math.h>
+#include <stdio.h>
 
 int main(){
-    double x = 0.5;
-    x = sin(x);
+    double x = sin(0.5);
+
+    // write to output.txt
+    FILE *fp = fopen("output.txt", "w");
+    if (fp == NULL) {
+        perror("Error opening file");
+        return -1;
+    }
+
+    fprintf(fp, "sin(0.5) = %f\n", x);
+    fclose(fp);
     return 0;
 }
