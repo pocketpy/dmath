cc7cdccae657b0f81a1e234eb9d4ed4c458bda0f
blueloveTH <blueloveTH@foxmail.com>
2026-01-07 16:16:08 +0800

    revert i386 support

--- include/linux/bits/alltypes.h
+++ include/linux/bits/alltypes.h
@@ -2,6 +2,8 @@
     #include "bits/x86_64/alltypes.h"
 #elif defined(__x32__)
     #include "bits/x32/alltypes.h"
+#elif defined(__i386__) || defined(_M_IX86)
+    #include "bits/i386/alltypes.h"
 #elif defined(__aarch64__)
     #include "bits/aarch64/alltypes.h"
 #elif defined(__arm__)
--- include/linux/bits/float.h
+++ include/linux/bits/float.h
@@ -4,6 +4,8 @@
     #include "../../../musl/arch/x86_64/bits/float.h"
 #elif defined(__x32__)
     #include "../../../musl/arch/x32/bits/float.h"
+#elif defined(__i386__) || defined(_M_IX86)
+    #include "../../../musl/arch/i386/bits/float.h"
 #elif defined(__aarch64__)
     #include "../../../musl/arch/aarch64/bits/float.h"
 #elif defined(__arm__)
